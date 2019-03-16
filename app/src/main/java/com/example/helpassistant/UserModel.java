71cfc454b62cd2a46dd71f22588772e258c89130
trast <mibrahim_elferga@hotmail.com>
2019-03-16 20:43:53 +0200

    UserModel Claas

---
+++ app/src/main/java/com/example/helpassistant/UserModel.java
@@ -0,0 +1,50 @@
+package com.example.helpassistant;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
+public class UserModel implements Serializable {
+    private String FirstName;
+    private String LastName;
+    private String UserName;
+    private String Email;
+    private String Phone;
+
+    public String getFirstName(){
+        return  this.FirstName;
+    }
+
+    public String getLastName(){
+        return this.LastName;
+    }
+
+    public String getUserName(){
+        return this.UserName;
+    }
+
+    public String getEmail(){
+        return this.Email;
+    }
+
+    public String getPhone(){
+        return this.Phone;
+    }
+
+    // Return an object from JSON
+    public static UserModel fromJson(JSONObject jsonObject){
+        UserModel user = new UserModel();
+        try{
+            user.Email = jsonObject.getString("Email");
+            user.FirstName = jsonObject.getString("FirstName");
+            user.LastName = jsonObject.getString("LastName");
+            user.Phone = jsonObject.getString("PhoneNumber");
+            user.UserName = jsonObject.getString("UserName");
+            return user;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
