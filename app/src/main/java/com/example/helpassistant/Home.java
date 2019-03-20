59c9e0f667a2de5285130a4a87cd6376eaa01c7d
AHmeed PC <ragbahmed1566@gmail.com>
2019-03-20 03:24:26 +0200

    add google maps

--- app/src/main/java/com/example/helpassistant/Home.java
+++ app/src/main/java/com/example/helpassistant/Home.java
@@ -1,12 +1,15 @@
 package com.example.helpassistant;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.time.Instant;
 
 public class Home extends AppCompatActivity {
 
@@ -42,5 +45,10 @@ public class Home extends AppCompatActivity {
     public void SC(View view) {
         Intent K = new Intent(Home.this,SetupContact.class);
         startActivity(K);
+    }
+
+    public void Browesers(View view) {
+        Intent BroweserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps"));
+        startActivity(BroweserIntent);
     }
 }
