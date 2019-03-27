5b8ae848ea384dcfc4e58f7cc5e8e3adcd5342c6
NadraMagdy <nadramagdy777@gmail.com>
2019-03-27 15:26:11 +0200

    Apply New password and Setup Contact Methods

--- app/src/main/java/com/example/helpassistant/ApplyNewPassword.java
+++ app/src/main/java/com/example/helpassistant/ApplyNewPassword.java
@@ -3,7 +3,21 @@ package com.example.helpassistant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class ApplyNewPassword extends AppCompatActivity {
 
@@ -14,6 +28,61 @@ public class ApplyNewPassword extends AppCompatActivity {
     }
 
     public void Login(View view) {
+
+        EditText txtNewPass = findViewById(R.id.txtNewPassword);
+        String NewPassword = txtNewPass.getText().toString();
+
+        StringEntity jsonObject = null;
+        JSONObject paramsJson = new JSONObject();
+        try {
+            paramsJson.put("NewPassword", NewPassword);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            jsonObject = new StringEntity(paramsJson.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        HttpUtils.post("api/user/UpdatePassword",jsonObject,"application/json",new JsonHttpResponseHandler()
+        {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("asd", "---------------- this is response : " + response);
+                try {
+                    //JSONObject serverResp = new JSONObject(response.toString());
+                    Toast.makeText(getApplicationContext(),"Please check your email to activate your account.", Toast.LENGTH_LONG).show();
+                }
+                catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject error) {
+                // Log error message
+                // to help solve any problems
+                Log.e("omg f android", statusCode + " " + throwable.getMessage());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String err, Throwable throwable) {
+
+                // Log error message
+                // to help solve any problems
+                Toast.makeText(getApplicationContext(), err, Toast.LENGTH_LONG).show();
+                Log.e("omg ff android", statusCode + " " + throwable.getMessage());
+            }
+
+        });
+
         Intent i = new Intent(ApplyNewPassword.this , Home.class);
         startActivity(i);
     }
--- app/src/main/java/com/example/helpassistant/SetupContact.java
+++ app/src/main/java/com/example/helpassistant/SetupContact.java
@@ -3,8 +3,21 @@ package com.example.helpassistant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class SetupContact extends AppCompatActivity {
 
@@ -14,9 +27,77 @@ public class SetupContact extends AppCompatActivity {
         setContentView(R.layout.activity_setup_contact);
     }
 
-    public void save(View view) {
-        Intent i = new Intent(SetupContact.this , Home.class);
-        startActivity(i);
+    public void save(View view)
+    {
+        // Get the Edit Text of the firstNumber object
+        EditText txtFirstNumber = findViewById(R.id.txtFirstNum);
+        // Get the value entered by the user
+        String FirstNumber = txtFirstNumber.getText().toString();
+        // Get the Edit Text of the SecondNumber object
+        EditText txtSecondNumber = findViewById(R.id.txtSecondNum);
+        // Get the value entered by the user
+        String SecondNumber = txtSecondNumber.getText().toString();
+        // Get the Edit Text of the thirdNumber object
+        EditText txtThirdNumber = findViewById(R.id.txtThirdNum);
+        // Get the value entered by the user
+        String ThirdNumber = txtThirdNumber.getText().toString();
+        // Get the Edit Text of the Message object
+        EditText txtMessage = findViewById(R.id.txtmessage);
+        // Get the value entered by the user
+        String Message = txtMessage.getText().toString();
 
-}
+        // Define the Request Params object to call the REST API
+        StringEntity jsonObject = null;
+        JSONObject paramsJson = new JSONObject();
+        try {
+            paramsJson.put("FirstNumber",FirstNumber );
+            paramsJson.put("SecondNumber", SecondNumber);
+            paramsJson.put("ThirdNumber", ThirdNumber);
+            paramsJson.put("Message", Message);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            jsonObject = new StringEntity(paramsJson.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        // Call the HttpUtil Class
+        HttpUtils.post("api/user/Emergency", jsonObject, "application/json", new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statueCode, Header[] headers, JSONObject response) {
+                Log.d("asd", "---------------- this is response : " + response);
+                try {
+                    //JSONObject serverResp = new JSONObject(response.toString());
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject error) {
+                // Log error message
+                // to help solve any problems
+                Log.e("omg android", statusCode + " " + throwable.getMessage());
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String err, Throwable throwable) {
+                // Log error message
+                // to help solve any problems
+                Toast.makeText(getApplicationContext(), err, Toast.LENGTH_LONG).show();
+                Log.e("omg android", statusCode + " " + throwable.getMessage());
+            }
+        });
+
+        Intent i = new Intent(SetupContact.this , MainActivity.class);
+        startActivity(i);
+    }
 }
